c81720efbf0ef82d15afc646dccf7c4e8b8be923
Carlos <carlosyllana@atmira.com>
2018-03-22 22:36:51 +0100

    Proyecto PooVueling calculadora final

---
+++ PooVuelingXunitTests/CalculadoraXunitTests.cs
@@ -0,0 +1,34 @@
+﻿using PooVueling;
+
+using Xunit;
+
+namespace PooVuelingXunitTests
+{
+    public class CalculadoraXunitTest { 
+         ICalculadora iCalculadora = new Calculadora();
+
+        [Fact]
+        public void DivisionTest()
+        {
+            Assert.True(iCalculadora.Division(2, 2) == 1);
+        }
+
+        [Fact]
+        public void MultiplicacionTest()
+        {
+            Assert.Equal(iCalculadora.Multiplicacion(2, 2) , 4);
+        }
+
+        [Fact]
+        public void RestaTest()
+        {
+            Assert.NotEqual(iCalculadora.Resta(2, 2) == 9 );
+        }
+
+        [Fact]
+        public void SumaTest()
+        {
+            Assert.False(iCalculadora.Suma(2, 2) == 4);
+        }
+    }
+}
